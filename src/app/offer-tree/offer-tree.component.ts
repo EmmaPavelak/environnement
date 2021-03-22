f376f3aef7dddff6a1c6a8f1880850952c46a335
Thomas Roque <thomas.roquedasilva@ynov.com>
2021-03-22 22:14:22 +0100

    Adding myAccount

---
+++ src/app/offer-tree/offer-tree.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-offer-tree',
+  templateUrl: './offer-tree.component.html',
+  styleUrls: ['./offer-tree.component.scss']
+})
+export class OfferTreeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
